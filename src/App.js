e63e24ee90bb1ea2bc0aedd399ecaae096cd7661
Md. Rakibul Hasan <rakib251193@gmail.com>
2020-08-25 05:19:16 +0600

    Learn Recat Basic Concept

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+let friends=[{Name:'Etu',Address:'Dhaka'},
+  {Name:'Shikha', Address:'Gazipur'},
+  {Name:'Mahbub', Address:'Jamalpur'},
+  {Name:'Shrabontee', Address:'Jamalpur'}
+  ]
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Done <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" >
+      <Counter></Counter>
+      {friends.map(friend=><Friend friend = {friend}></Friend>)}
+      </div>
   );
+
+}
+function Counter(){
+  const[count,setCount]=useState(0);
+  // const handleiIncrease = ()=> {
+  //   const newCount =count+1;
+  //   setCount(newCount);
+  // }
+  return(
+  <div>
+    <h2>Count :{count}</h2>
+    <button style={{padding:'10px 20px', color:'white',backgroundColor:'green',border:'0px',borderRadius:'10px',margin:'10px'}} onClick={()=>setCount(count+1)}>Increase</button>
+    <button style={{padding:'10px 20px', color:'white',backgroundColor:'red',border:'0px',borderRadius:'10px'}} onClick={()=>{
+    if(count>0){
+      setCount(count-1)
+    }
+    }}>Decrease</button>
+    
+    </div>
+  )
 }
 
+function Friend(props){
+  const personStyle={
+    float:'left',
+    width:'30%',
+    padding:'10px',
+    backgroundColor:'lightgray',
+    height:'200px',
+    margin:'10px'
+  }
+  const {Name,Address}=props.friend;
+  return(
+    <div style={personStyle}>
+      <h2>{Name}</h2>
+      <h4>{Address}</h4>
+    </div>
+  )
+}
 export default App;
